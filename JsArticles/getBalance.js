a1b9490f05150e8a277c2e89b0e962b9b964823b
Ariel Kollross <arielkollross@gmail.com>
2020-07-05 22:15:13 -0300

    first article

---
+++ JsArticles/getBalance.js
@@ -0,0 +1,47 @@
+const transactions = [
+  {
+    title: "Salário",
+    value: 3000,
+    type: "income"
+  },
+  {
+    title: "Salário",
+    value: 3000,
+    type: "income"
+  },
+  {
+    title: "Salário",
+    value: 2000,
+    type: "outcome"
+  },
+  // {
+  //   title: "Salário",
+  //   value: 500,
+  //   type: "outcome"
+  // },
+]
+
+console.log(transactions);
+
+
+function getBaleance(){
+  const income = transactions
+  .filter(transactions => transactions.type === 'income')
+  .reduce((sum, current) => {
+   return sum + current.value
+  }, 0);
+
+const outcome = transactions
+  .filter(transactions => transactions.type === 'outcome').reduce((sum, acc) => {return sum + acc.value}, 0);
+
+console.log(outcome);
+
+  return {
+    income,
+    outcome,
+    total: income - outcome
+  };
+}
+
+console.log(getBaleance());
+
